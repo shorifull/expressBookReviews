23c38c4f409f63adc9a67cb7b0288e79d6cbebe6
Ratan Mia <shorifull@gmail.com>
2023-09-02 00:40:00 +0600

    Task- Review added and deleted done

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -47,8 +47,36 @@ regd_users.post("/login", (req, res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  const review = req.body.review;
+  const username = req.session.authorization.username;
+  if (books[isbn]) {
+      let book = books[isbn];
+      book.reviews[username] = review;
+      return res.status(200).send("Review successfully posted");
+  }
+  else {
+      return res.status(404).json({message: `ISBN ${isbn} not found`});
+  }
 });
+
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+  if (books[isbn]) {
+      let book = books[isbn];
+      delete book.reviews[username];
+      return res.status(200).send("Review successfully deleted");
+  }
+  else {
+      return res.status(404).json({message: `ISBN ${isbn} not found`});
+  }
+});
+
+
+
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;

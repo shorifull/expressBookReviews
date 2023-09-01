3eb90d3e856befc56db7bbb473acab7da40aa36c
Ratan Mia <shorifull@gmail.com>
2023-09-01 23:11:21 +0600

    Task 5: Completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -75,7 +75,7 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const ISBN = req.params.isbn;
+  res.send(books[ISBN].reviews)
 });
-
 module.exports.general = public_users;

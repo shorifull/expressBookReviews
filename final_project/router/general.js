5a82448d9090c4f8cc91f32f98a373aa582b32d9
Ratan Mia <shorifull@gmail.com>
2023-09-01 22:25:15 +0600

    Task 2- ISBN done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,15 +23,15 @@ public_users.get('/',function (req, res) {
   )
 
 
-
-
   // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const ISBN = req.params.isbn;
+  res.send(books[ISBN]);
  });
   
 // Get book details based on author
